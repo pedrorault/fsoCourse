a24d5af3b4af9e668626dcad3b9179dea00c9936
pedrorault <52670501+pedrorault@users.noreply.github.com>
2020-03-24 14:57:28 -0300

    Part1 Exercise 1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,32 +1,61 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Anecdote = ({anecdotes,selected,votes,text}) =>{
+  return(
+    <div>
+      <h1>{text}</h1>
+      <p style={{height:25+'px'}}>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+    </div>
+  )
+}
 
 const Button = ({onClick, text}) => {
   return(
     <button onClick={onClick}>{text}</button>
   )
 }
-const App = (props) => {
-  const rand = (selected) => {
-    let random;
-    do{
-      random = parseInt(Math.random()*10);
-    }while(random > (anecdotes.length-1) || random === selected)
-    return random
+
+const MostVoted = (props) =>{
+  const arr = props.anecdoteProp.votes
+  const indexMostVoted = arr.indexOf(Math.max(...arr))
+  if(indexMostVoted === 0){
+    return(<></>)
   }
-  const [selected, setSelected] = useState(rand)
+  return(
+    <div>
+    <Anecdote {...props.anecdoteProp} selected={indexMostVoted} text ={"Anecdote with most votes"}/>
+    </div>
+  )
+}
+
+const randomSelectIndexAnecdote = (selected) => {
+  const ifSelected = (selected === undefined) ? -1 : selected;
+  let random;
+    do{
+      random = Math.floor(Math.random()*10);
+    }while(random > (anecdotes.length-1) || random === ifSelected )
+    return random  
+}
+
+const onClickVote = (selected, votes, setVotes) => {
+  const newVote = [...votes]; 
+  newVote[selected]+=1; 
+  setVotes(newVote);
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(randomSelectIndexAnecdote)
   const [votes, setVotes] = useState(Array(6).fill(0))
 
-
-  
+  const anecdoteProp = {anecdotes:props.anecdotes, votes:votes, selected:selected}
   return (
     <div>
-      <p style={{height:25+'px'}}>{props.anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-
-      <Button text="vote" onClick={() => {const newVote = [...votes]; newVote[selected]+=1; setVotes(newVote)}} />
-      <Button text="next anedocte" onClick={()=>setSelected(rand)} />
+      <Anecdote {...anecdoteProp} text={"Anecdote of the day"}/>
+      <Button text="vote" onClick={() => onClickVote(selected,votes,setVotes)} />
+      <Button text="next anedocte" onClick={()=>setSelected(randomSelectIndexAnecdote(selected))} />
+      <MostVoted anecdoteProp={anecdoteProp} />
     </div>
   )
 }
