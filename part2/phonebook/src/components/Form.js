74cfdf6e0749d8ea3c62c0e15a73388258e0f85f
pedrorault <pedrorault@gmail.com>
2020-04-03 02:19:22 -0300

    Changed how errors were handled

--- part2/phonebook/src/components/Form.js
+++ part2/phonebook/src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState} from 'react';
 import contactService from '../services/contacts'
 
 const Form = ({persons,setPersons,setAlertMsg}) => {
@@ -10,23 +10,23 @@ const Form = ({persons,setPersons,setAlertMsg}) => {
   
     const onSubmitPhonebook = (event) =>{
       event.preventDefault()
-      if(newName === '' || newNumber === ''){ 
-        setAlertMsg({isError:true,message:`One or more fields empty`})
-        setTimeout(()=>setAlertMsg({isError:false,message:""}),3000)
-        return null
-      }
-  
       const person = persons.find((person) => person.name === newName)
       if(person !== undefined){ 
-        if(window.confirm(`${person.name} is already on the list, replace old number with a new one?`)){
+        const confirm = window.confirm(`${person.name} is already on the list, replace old number with a new one?`)
+        if(confirm){
           contactService.updateContact({...person,number:newNumber}).then(()=>{
-            setPersons(persons.map(per=>(per.id===person.id)?{...per,number:newNumber}:per))        
+            setPersons(persons.map(per=>(per.id===person.id)?{...per,number:newNumber}:per)) 
+
             setAlertMsg({isError:false,message:`Updated ${newName}`})
             setTimeout(()=>setAlertMsg({isError:false,message:""}),3000)        
+            setNewName('')
+            setNewNumber('')
+
           })
-          .catch( (e) => {
-            setAlertMsg({isError:true,message:`${e}`})
-            setTimeout(()=>setAlertMsg({isError:false,message:""}),3000) 
+          .catch(error => {
+            const msg =  error.response.data.error || error.response.data.message 
+            setAlertMsg({isError:true,message:`${msg}`})
+            setTimeout(()=>setAlertMsg({isError:false,message:""}),5000)
           })
         }      
       }else{
@@ -35,14 +35,15 @@ const Form = ({persons,setPersons,setAlertMsg}) => {
             setPersons(persons.concat(v))
             setAlertMsg({isError:false,message:`Added ${newName}`})
             setTimeout(()=>setAlertMsg({isError:false,message:""}),3000)
+            setNewName('')
+            setNewNumber('')
           })   
-          .catch(e=>{
-            setAlertMsg({isError:true,message:`${e}`})
-            setTimeout(()=>setAlertMsg({isError:false,message:""}),3000)
+          .catch(error=>{
+            const msg = error.response.data.message || error.response.data.error
+            setAlertMsg({isError:true,message:`${msg}`})
+            setTimeout(()=>setAlertMsg({isError:false,message:""}),5000)
           })
       }
-      setNewName('')
-      setNewNumber('')
     }
     return (
       <div>
