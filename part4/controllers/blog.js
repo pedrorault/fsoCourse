b9be3b3c5a2f675c1f1f093431012b080502e55b
pedrorault <pedrorault@gmail.com>
2020-04-11 16:57:50 -0300

    Part4 Exercises 4.9 to 4.14

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -1,7 +1,6 @@
 const blogRouter = require('express').Router()
 require('express-async-errors')
 const Blog  = require('../models/blog')
-const logger = require('../utils/logger')
 
 blogRouter.get('/',async (request, response) => {
   const blogs = await Blog.find({})
@@ -14,13 +13,33 @@ blogRouter.get('/:id',async (request,response) => {
 })
 
 blogRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
-  const savedBlog = await blog.save()  
-  response.json(savedBlog.toJSON())
+  const blog = {
+    ...request.body,
+    likes: request.body.likes || 0
+  }
+  if(!blog.title && !blog.url){
+    return response.status(400).json({error: 'No title, No url'})
+  }  
+  const savedBlog = await new Blog(blog).save()  
+  response.status(201).json(savedBlog.toJSON())
 })
+
+blogRouter.delete('/:id', async (request,response) => {
+  const blog = await Blog.findById(request.params.id)
+  await blog.remove()
+  response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const reqBlog = request.body
+  const blog = await Blog.findByIdAndUpdate(request.params.id, reqBlog, {new:true})
+  response.status(201).json(blog.toJSON())
+})
+
 blogRouter.get('*', (request,response,next) => {
   const error = new Error()
   error.status = 404
   next(error)
 })
+
 module.exports = blogRouter
--- part4/tests/persons_api.test.js
+++ part4/tests/persons_api.test.js
@@ -2,26 +2,14 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const helper = require('./test_helper')
 const api = supertest(app)
 
-const initialBlogs = [
-  {
-    title: 'Somebody',
-    author: 'Pedro',
-    url: 'http://',
-    likes: 10
-  },
-  {
-    title: 'once told me',
-    author: 'Pedro',
-    url: 'http://',
-    likes: 100
-  }
-]
+
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  const blogObjects = initialBlogs.map(blog => new Blog(blog))
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
   const promiseArray = blogObjects.map(blog => blog.save())
   await Promise.all(promiseArray)
 })
@@ -35,13 +23,54 @@ test('blogs are returned as json', async ()=>{
 })
 test('there are two blogs', async () => {
   const response = await api.get('/blogs')
-  expect(response.body).toHaveLength(initialBlogs.length)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 test('the first blog is Something', async () => {
   const response = await api.get('/blogs')
-  expect(response.body[0].title).toBe(initialBlogs[0].title)
+  expect(response.body[0].title).toBe(helper.initialBlogs[0].title)
 })
+
+
+test('the unique identifier of blog is id', async () => {
+  const response = await api.get('/blogs')
+  const blog = response.body[0]
+  expect(blog.id).toBeDefined()
+})
+
+describe('New Blog Tests', () => {
+  test('Creating a new blog', async () => {
+    const blog = {
+      title: 'NewContent',
+      author: 'Pedro',
+      url: 'http://',
+      likes: 10
+    }
+    await api.post('/blogs').send(blog).expect(201).expect('Content-Type','application/json; charset=utf-8')
+    const blogAtEnd = await helper.blogsInDb()
+    expect(blogAtEnd).toHaveLength(helper.initialBlogs.length +1)
+  
+    const content = blogAtEnd.map(blog=>blog.title)
+    expect(content).toContain(blog.title)
+  })
+  test('Missing like property gives 0 likes', async () => {
+    const blog = {
+      title: 'NewContent',
+      author: 'Pedro',
+      url: 'http://'
+    }
+    const newBlog = await api.post('/blogs').send(blog).expect(201)
+    expect(newBlog.body.likes).toBe(0)  
+    
+  })
+  test.only('Missing title and url gives 404', async () => {
+    const blog = {
+      author: 'Pedro',
+    }
+    await api.post('/blogs').send(blog).expect(400)
+  })
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
