4956a919b8ea07052a533a27841418dd9d529b38
pedrorault <pedrorault@gmail.com>
2020-04-30 11:53:12 -0300

    Extracted all into components and removed ArrowFunc from render

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,18 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
 import blogService from './services/blogs'
 import Login from './components/LoginForm'
 import BlogForm from './components/BlogForm'
+import BlogList from './components/BlogList'
 
 const App = () => {
-  const [user,setUser] = useState(null)
-  const [blogs, setBlogs] = useState([])
-  
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+  const [user,setUser] = useState(null)  
 
   useEffect(()=>{
     const loggedUser = window.localStorage.getItem('loggedUser')
@@ -23,20 +16,12 @@ const App = () => {
     }
   },[])
 
-  const blogList = () => {
-    return(
-      <div>
-        <h2>blogs</h2>
-        {blogs.map(blog=><Blog key = {blog.id} blog={ blog } />)}
-      </div>
-    )      
-  }
   
   return (    
     <div>
       <Login handleSetUser={(x)=> setUser(x)} user={user}/>
       {user != null ? <BlogForm /> : null}
-      {user != null ? blogList() : null}
+      {user != null ? <BlogList /> : null}
     </div>
   )
 }
